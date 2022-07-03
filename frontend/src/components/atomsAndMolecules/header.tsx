58558f64f8362ae5c4653e73d39381d6cdd1d36a
TheSelfAtu <43713568+TheSelfAtu@users.noreply.github.com>
2022-07-03 20:56:34 +0900

    A: ナビゲーションバーを追加 (#9)
    * A: redirect設定を追加
    
    * A: ヘッダーを追加

---
+++ frontend/src/components/atomsAndMolecules/header.tsx
@@ -0,0 +1,61 @@
+import React from "react";
+import {
+  Box,
+  Stack,
+  Heading,
+  Flex,
+  Text,
+  Button,
+  useDisclosure,
+} from "@chakra-ui/react";
+import { HamburgerIcon } from "@chakra-ui/icons";
+
+const Header = (props) => {
+  const { isOpen, onOpen, onClose } = useDisclosure();
+  const handleToggle = () => (isOpen ? onClose() : onOpen());
+
+  return (
+    <Flex
+      as="nav"
+      align="center"
+      justify="space-between"
+      wrap="wrap"
+      padding={2}
+      bg="teal.500"
+      color="white"
+      {...props}
+    >
+      <Box display={{ base: "block", md: "none" }} onClick={handleToggle}>
+        <HamburgerIcon />
+      </Box>
+
+      <Stack
+        direction={{ base: "column", md: "row" }}
+        display={{ base: isOpen ? "block" : "none", md: "flex" }}
+        width={{ base: "full", md: "auto" }}
+        alignItems="center"
+        flexGrow={1}
+        mt={{ base: 4, md: 0 }}
+        ml={4}
+        spacing={10}
+      >
+        <Text>マイページ</Text>
+        <Text>エンジニア</Text>
+      </Stack>
+
+      <Box
+        display={{ base: isOpen ? "block" : "none", md: "block" }}
+        mt={{ base: 4, md: 0 }}
+      >
+        <Button
+          variant="outline"
+          _hover={{ bg: "teal.700", borderColor: "teal.700" }}
+        >
+          ログイン
+        </Button>
+      </Box>
+    </Flex>
+  );
+};
+
+export default Header;
