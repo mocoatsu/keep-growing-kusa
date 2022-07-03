d2a728ad3b8fcba1761ac1c1feb0dbe2ff1c8ad2
TheSelfAtu <r673774d72k77e@gmail.com>
2022-07-03 20:53:24 +0900

    A: ヘッダーを追加

--- frontend/pages/_app.tsx
+++ frontend/pages/_app.tsx
@@ -2,13 +2,15 @@ import { ChakraProvider } from "@chakra-ui/react";
 
 import "../styles/globals.css";
 import type { AppProps } from "next/app";
+import Header from "../src/components/atomsAndMolecules/header";
 
-function MyApp({ Component, pageProps }: AppProps) {
+function App({ Component, pageProps }: AppProps) {
   return (
     <ChakraProvider>
+      <Header />
       <Component {...pageProps} />;
     </ChakraProvider>
   );
 }
 
-export default MyApp;
+export default App;
