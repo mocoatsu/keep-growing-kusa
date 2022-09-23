2a1349d7c174526e103b4d7e29858679ef31482d
TheSelfAtu <r673774d72k77e@gmail.com>
2022-09-23 23:06:32 +0900

    A: Achievementドメインを修正

--- frontend/src/domain/Achievement.ts
+++ frontend/src/domain/Achievement.ts
@@ -5,13 +5,19 @@ export type AchievementType = {
   difficultyLevel: number;
 };
 
+const defaultValue: AchievementType = {
+  id: 0,
+  name: "",
+  description: "",
+  difficultyLevel: 0,
+};
 export class Achievement {
-  public readonly id: number;
+  public readonly id;
   public readonly name: string;
   public readonly description: string;
   public readonly difficultyLevel: number;
 
-  constructor(v: AchievementType) {
+  constructor(v: AchievementType = defaultValue) {
     this.id = v.id;
     this.name = v.name;
     this.description = v.description;
