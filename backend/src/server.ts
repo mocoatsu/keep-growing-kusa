81b7d529d839f4d22044959380d193a757f68fd0
TheSelfAtu <r673774d72k77e@gmail.com>
2022-10-08 22:21:35 +0900

    A: loggerを追加

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,5 +1,8 @@
 import express from "express";
 import cors from "cors";
+import morgan from "morgan";
+
+import { router as authRouter } from "./presentation/routes/authController";
 import { router as achievementRouter } from "./presentation/routes/achievementController";
 import { router as unlockAchievementRouter } from "./presentation/routes/unloclAchievementController";
 import { router as contributionController } from "./presentation/routes/contributionController";
@@ -14,6 +17,8 @@ app.listen(port, () => {
 app
   .use(cors())
   .use(express.json())
+  .use(morgan("short"))
+  .use("/auth", authRouter)
   .use("/achievements", achievementRouter)
   .use("/unlocAchievements", unlockAchievementRouter)
   .use("/contributions", contributionController);
