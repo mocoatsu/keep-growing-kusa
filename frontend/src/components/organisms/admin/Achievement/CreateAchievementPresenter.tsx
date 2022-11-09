7d6b74aaeaf52379df188a1814ae79b9c8b70a5c
TheSelfAtu <r673774d72k77e@gmail.com>
2022-11-09 20:07:22 +0900

    F: react-hook-formがundefinedになるバグを修正

--- frontend/src/components/organisms/admin/Achievement/CreateAchievementPresenter.tsx
+++ frontend/src/components/organisms/admin/Achievement/CreateAchievementPresenter.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { useForm, SubmitHandler } from "react-hook-form";
+import { UseFormHandleSubmit, UseFormRegister } from "react-hook-form";
 import {
   FormControl,
   FormLabel,
@@ -12,26 +12,19 @@ import {
 
 import { Button } from "../../../atomsAndMolecules/Button";
 import { H1 } from "../../../atomsAndMolecules/Heading";
-import { createAchievement } from "../../../../infrastructure/express/api";
+import { RequestAchievement } from "../../../../infrastructure/express/api";
 
-type Inputs = {
-  name: string;
-  description: string;
-  difficultyLevel: number;
-};
-
-export const CreateAchievementPresenter = () => {
-  const {
-    register,
-    handleSubmit,
-    formState: { errors },
-  } = useForm<Inputs>();
-  const onSubmit: SubmitHandler<Inputs> = (v) => createAchievement(v);
-
+export const CreateAchievementPresenter = ({
+  ...formProps
+}: {
+  handleSubmit: UseFormHandleSubmit<RequestAchievement>;
+  onSubmit: (v: RequestAchievement) => any;
+  register: UseFormRegister<RequestAchievement>;
+}) => {
   return (
     <Container maxWidth={"container.xl"} h="calc(100vh)">
       <Box backgroundColor={"white"} padding={"2em"} mt={"3em"}>
-        <form onSubmit={handleSubmit(onSubmit)}>
+        <form onSubmit={formProps.handleSubmit(formProps.onSubmit)}>
           <VStack>
             <H1>実績新規作成</H1>
             <HStack>
@@ -39,7 +32,7 @@ export const CreateAchievementPresenter = () => {
                 <FormLabel>実績名</FormLabel>
                 <Input
                   type="text"
-                  {...(register("name"), { required: true })}
+                  {...formProps.register("name", { required: true })}
                   variant={"outline"}
                   outlineColor={"#A0AEC0"}
                 />
@@ -48,7 +41,7 @@ export const CreateAchievementPresenter = () => {
                 <FormLabel>実績説明</FormLabel>
                 <Input
                   type="text"
-                  {...(register("description"), { required: true })}
+                  {...formProps.register("description", { required: true })}
                   variant={"outline"}
                   outlineColor={"#A0AEC0"}
                 />
@@ -57,14 +50,11 @@ export const CreateAchievementPresenter = () => {
                 <FormLabel>実績解除難易度</FormLabel>
                 <Input
                   type="text"
-                  {...(register("difficultyLevel"), { required: true })}
+                  {...formProps.register("difficultyLevel", { required: true })}
                   variant={"outline"}
                   outlineColor={"#A0AEC0"}
                 />
               </FormControl>
-              {errors.name && errors.description && errors.difficultyLevel && (
-                <span>入力されていない項目があります</span>
-              )}
             </HStack>
             <Button type="submit">作成</Button>
           </VStack>
