6e3fc5ee7fc525c2197b303b9e20608891e30692
TheSelfAtu <r673774d72k77e@gmail.com>
2022-11-14 20:30:00 +0900

    A: 解除実績を取得するコントローラを作成

---
+++ backend/src/presentation/routes/unlockAchievementController.ts
@@ -0,0 +1,32 @@
+import express from "express";
+import { UnlockAchievementApplicationService } from "../../application/unlockAchievement/unlockAchievementApplicationService";
+import { UnlockAchievementRepository } from "../../domain/models/unlockAchievement/unlockAchievementRepository";
+
+export const router = express.Router();
+
+router
+  .get("/:engineerId", async (req, res) => {
+    const unlockAchievementApplicationService =
+      new UnlockAchievementApplicationService(
+        new UnlockAchievementRepository()
+      );
+
+    const unlockedAchievements =
+      await unlockAchievementApplicationService.getAllUnlockedAchievements(
+        Number(req.params.engineerId)
+      );
+    res.json(unlockedAchievements);
+  })
+  .post("/delete", async (req, res) => {
+    const unlockAchievementApplicationService =
+      new UnlockAchievementApplicationService(
+        new UnlockAchievementRepository()
+      );
+
+    await unlockAchievementApplicationService.deleteUnlockAchievements(
+      req.body.engineerId,
+      req.body.achievementId
+    );
+
+    res.json({ message: "解除された実績の削除に成功しました" });
+  });
