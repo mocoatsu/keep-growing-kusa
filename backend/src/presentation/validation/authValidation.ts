f612333a9ca14de4c89a081381ba221c7f9a23f3
TheSelfAtu <r673774d72k77e@gmail.com>
2022-11-28 21:59:13 +0900

    I: validationを修正

--- backend/src/presentation/validation/authValidation.ts
+++ backend/src/presentation/validation/authValidation.ts
@@ -5,8 +5,6 @@ const engineer = z.object({
   password: z.string().min(1),
 });
 
-export type User = z.infer<typeof engineer>;
-
-export function signUpValidation(name: string, password: string) {
+export function authValidation(name: string, password: string) {
   return engineer.parse({ name: name, password: password });
 }
