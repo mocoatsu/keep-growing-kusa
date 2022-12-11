3b1b9d6927a30e4f35255eb0bdfe986b11cd27fe
TheSelfAtu <r673774d72k77e@gmail.com>
2022-12-11 22:05:02 +0900

    A: modalを追加

---
+++ frontend/src/components/atomsAndMolecules/Modal.tsx
@@ -0,0 +1,45 @@
+import {
+  Modal as ChakraModal,
+  ModalOverlay,
+  ModalContent,
+  ModalHeader,
+  ModalFooter,
+  ModalBody,
+  ModalCloseButton,
+} from "@chakra-ui/react";
+import { Button } from "./Button";
+
+export function Modal({
+  label,
+  content,
+  isOpen,
+  onOpen,
+  onClose,
+}: {
+  label: string;
+  content: string;
+  isOpen: boolean;
+  onOpen: () => void;
+  onClose: () => void;
+}) {
+  return (
+    <>
+      <ChakraModal isOpen={isOpen} onClose={onClose}>
+        <ModalOverlay />
+        <ModalContent>
+          <ModalHeader>{label}</ModalHeader>
+          <ModalCloseButton />
+          <ModalBody>
+            <p>{content}</p>
+          </ModalBody>
+
+          <ModalFooter>
+            <Button colorScheme="blue" mr={3} onClick={onClose}>
+              閉じる
+            </Button>
+          </ModalFooter>
+        </ModalContent>
+      </ChakraModal>
+    </>
+  );
+}
