9ee624f845019a9c0b84f584f38336364a44c561
TheSelfAtu <r673774d72k77e@gmail.com>
2022-12-12 19:05:09 +0900

    A: モーダルをcontainerから呼び出す処理を追加

--- frontend/src/components/organisms/AchievementContainer.tsx/AchievementContainer.tsx
+++ frontend/src/components/organisms/AchievementContainer.tsx/AchievementContainer.tsx
@@ -1,13 +1,26 @@
+import { useDisclosure } from "@chakra-ui/react";
 import { useAchievements } from "../../../hooks/useAchievements";
 import { useUnlockAchievements } from "../../../hooks/useUnlockAchievements";
+import { Modal } from "../../atomsAndMolecules/Modal";
 import { AchievementPresenter } from "./AchievementPresenter";
 
 export const AchievementContainer = () => {
   const { achievements } = useAchievements();
   const { newUnlockedAchievements, unlockedAchievements } =
     useUnlockAchievements();
+  const { isOpen, onClose } = useDisclosure({ defaultIsOpen: true });
 
   return (
-    <AchievementPresenter achievements={achievements}></AchievementPresenter>
+    <>
+      <AchievementPresenter achievements={achievements}></AchievementPresenter>
+      {newUnlockedAchievements.length > 0 && (
+        <Modal
+          label="実績解除"
+          content="解除実績"
+          isOpen={isOpen}
+          onClose={onClose}
+        />
+      )}
+    </>
   );
 };
